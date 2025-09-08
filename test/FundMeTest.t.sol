022dcf582565248d5bb4424f96b9f7b13de6bf75
Shivam Taneja <shiivamtaneja@gmail.com>
2025-09-08 18:48:19 +0000

    add: simple test
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,18 @@
+// SPD-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {FundMe} from "../src/FundMe.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollaIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
